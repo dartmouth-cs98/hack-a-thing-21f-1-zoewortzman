233782eb4aece24e726c863802049018cf231e06
Zoe Wortzman <zoewortzman@Zoes-MacBook-Pro.local>
2021-09-22 11:46:04 -0400

    Navigation bar and commerce API key information

--- src/components/Products/products.jsx
+++ src/components/Products/products.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles'
 
 const products = [
-    {id: 1, name: 'Shoes', description: 'running shoes.', price: '$50'},
-    {id: 2, name: 'watch', description: 'gold.', price: '$50'},
+    {id: 1, name: 'Shoes', description: 'running shoes.', price: '$50', image: 'https://images.app.goo.gl/V27qX3ej3oynh17B9'},
+    {id: 2, name: 'Watch', description: 'gold.', price: '$50'},
 ];
 
 const Products = () => {
+    const classes = useStyles();
     return (
-        <main>
-        <Grid container justify="center" spacing={4}>
-            {products.map((product) => (
-                <Grid item key={product.id} xs={12} md={4} lg={3}>
-                    <Product product={product}/>
-                </Grid>
-            ))}
-        </Grid>
-    </main>
+        <main className={classes.content} >
+            <div className={classes.toolbar}/>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} md={4} lg={3}>
+                        <Product product={product}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
     );
 }
 
