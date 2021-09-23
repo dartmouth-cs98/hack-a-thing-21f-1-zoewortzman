cf8cc18ae9e47ee4eb858709757ae051ed236cfd
Zoe Wortzman <zoewortzman@Zoes-MacBook-Pro.local>
2021-09-22 20:45:26 -0400

    Functionality of adding/removing to shopping cart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Container, Typography, Button, Grid } from '@material-ui/core'
+import { Link } from 'react-router-dom';
+import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
+
+
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
+    const isEmpty = cart.lines.items.length === 0;
+    const classes = useStyles();
+
+    const EmptyCart = () => (
+        <Typography variant="subtitle1">You Have no items in your cart, start shopping! 
+            <Link to="/" className={classes.link}>start adding some</Link>
+        </Typography>
+    )
+
+    const FilledCart = () => (
+        <>
+            <Grid container spacing={3}>
+                {cart.line.items.map((item) => (
+                    <Grid item xs={12} sm={4} key={item.id}>
+                        <CartItem itme={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart}/>
+                    </Grid>
+                ))} 
+            </Grid>
+            <div className={classes.cardDetails}>
+                <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol} </Typography>
+                <div>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart} > Empty Cart</Button>
+                    <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primart">Check Out</Button>
+
+                </div>
+            </div>
+        </>
+
+    )
+    return (
+        <Container>
+            <div className={classes.toolbar} />
+            <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
+            { isEmpty ? <EmptyCart /> : <FilledCart />}
+        </Container>
+    )
+}
+
+export default Cart
